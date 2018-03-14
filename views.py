0950d738f0548652051758a5fba4f0c7148f17ee
MurphyWan <MurphyWan@users.noreply.github.com>
2018-03-14 10:45:06 +0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,89 @@
+# coding:utf8
+# 4定义admin视图;下一步定义app模块下的初始化文件__init__.py
+
+# 从当前模块中导入
+from . import admin
+from flask import render_template, redirect, url_for
+
+
+# @admin.route("/")
+# def index():
+#     return "<h1 style='color:red'>this is admin </h1>"
+
+@admin.route("/")
+def index():
+    return render_template("admin/index.html")
+
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+# 5-3 5'30
+@admin.route("/preview/add/")
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+@admin.route("/preview/list/")
+def preview_list():
+    return render_template("admin/preview_list.html")
+
+#5-4
+@admin.route("/user/list/")
+def user_list():
+    return render_template("admin/user_list.html")
+
+@admin.route("/user/view/")
+def user_view():
+    return render_template("admin/user_view.html")
+
+#5-4
+@admin.route("/comment/list/")
+def comment_list():
+    return render_template("admin/comment_list.html")
+
+
+@admin.route("/moviecol/list/")
+def moviecol_list():
+    return render_template("admin/moviecol_list.html")
+
+#5-5
+@admin.route("/oplog/list/")
+def oplog_list():
+    return render_template("admin/oplog_list.html")
+
+@admin.route("/adminloginlog/list/")
+def adminloginlog_list():
+    return render_template("admin/adminloginlog_list.html")
+
+@admin.route("/userloginlog/list/")
+def userloginlog_list():
+    return render_template("admin/userloginlog_list.html")
+
